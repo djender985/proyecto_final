31e272979f3b9fbc98d9915f265967737594db28
djender985 <ing.djender@hotmail.com>
2023-08-14 22:28:28 -0300

    Cambios al test

--- blog/tests.py
+++ blog/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from blog.models import Articulo
-
+from django.contrib.auth.models import User
 
 class ArticuloTests(TestCase):
     def test_creacion_articulo(self):
@@ -10,27 +10,35 @@ class ArticuloTests(TestCase):
             titulo = 'Curiosidades del mundo',
             subtitulo = 'Como es posible que sucedan estas cosas?',
             cuerpo = 'Alguna vez que has preguntado como fue que construyeron las piramides, o como es que a veces hay relampago sin trueno?' ,
-            fecha = '2023-08-13 18:06:43.622106',
+            fecha = '2023-08-13 18:06:43',
             imagen = 'tile_img/oppenhaimer_pic_zbxA844.jpg',
         )
         articulo.save()
 
         # Compruebo que el curso fue creado y la data fue guardad correctamente
         self.assertEqual(Articulo.objects.count(), 1)
-        self.assertEqual(Articulo.autor, 'Pedro Rojas')
-        self.assertEqual(Articulo.titulo, 'Curiosidades del mundo')
-        self.assertEqual(Articulo.subtitulo, 'Como es posible que sucedan estas cosas?')
-        self.assertEqual(Articulo.cuerpo, 'Alguna vez que has preguntado como fue que construyeron las piramides, o como es que a veces hay relampago sin trueno?')
-        self.assertEqual(Articulo.fecha, '2023-08-13 18:06:43.622106')
-        self.assertEqual(Articulo.imagen, 'tile_img/oppenhaimer_pic_zbxA844.jpg')
+        self.assertEqual(articulo.autor, 'Pedro Rojas')
+        self.assertEqual(articulo.titulo, 'Curiosidades del mundo')
+        self.assertEqual(articulo.subtitulo, 'Como es posible que sucedan estas cosas?')
+        self.assertEqual(articulo.cuerpo, 'Alguna vez que has preguntado como fue que construyeron las piramides, o como es que a veces hay relampago sin trueno?')
+        self.assertEqual(articulo.fecha, '2023-08-13 18:06:43')
+        self.assertEqual(articulo.imagen, 'tile_img/oppenhaimer_pic_zbxA844.jpg')
 
-    def test_curso_str(self):
+    def test_articulo_str(self):
+        user = User(
+            username = 'pedro55',
+            first_name = 'Pedro',
+            last_name = 'Rojas',
+            email = 'pedrorojas@hotmail.com',
+        )
+        user.save()
+        
         articulo = Articulo(
-            autor="Pedro Rojas", 
-            titulo='Curiosidades del mundo', 
+            autor=user,
+            titulo='Curiosidades del mundo',
             subtitulo='Como es posible que sucedan estas cosas?',
             cuerpo='Alguna vez que has preguntado como fue que construyeron las piramides, o como es que a veces hay relampago sin trueno?',
-            fecha='2023-08-13 18:06:43.622106',
+            fecha='2023-08-13 18:06:43',
             imagen='tile_img/oppenhaimer_pic_zbxA844.jpg'
             )
         articulo.save()
